4cebb3a6123c07532176bbdd559c6bec24e1af66
Ashwini Karke <ashwinik6112000@gmail.com>
2025-04-08 12:14:11 +0530

    Update index.tsx

--- src/Components/EditProfile/index.tsx
+++ src/Components/EditProfile/index.tsx
@@ -59,7 +59,7 @@ function EditProfile(props: any) {
   async function ChangePassword() {
     const name = localStorage.getItem("username");
     const emailId = localStorage.getItem("email");
-    const userId = localStorage.getItem("id") || 0;
+    const userId = Number(localStorage.getItem("id"));
     const userPassword = localStorage.getItem("password");
     // setUsername(name? name:"Ashwini")
     // setEmail(email? email:"ashwini@123")
